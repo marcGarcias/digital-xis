d9699ccbf433de9aa869f8d3f82c115722ebcab7
Marcelo Garcias <garcias.marcelo98@proton.me>
2026-03-15 09:29:47 -0300

    feat(environment): configura variáveis de ambiente e servidor SSR
    Configurações técnicas de execução e ambiente:
    - Environments: Definições para desenvolvimento e produção.
    - SSR: Scripts de servidor e pontos de entrada para renderização no lado do servidor (Angular Universal).

---
+++ front-end/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://digitalxis.com.br/api',
+    serverUrl: 'https://digitalxis.com.br',
+    turnstileSiteKey: '0x4AAAAAACq-vuBovbvO9j03',
+    googleAnalyticsId: 'G-6KLYZQT3SV'
+};
