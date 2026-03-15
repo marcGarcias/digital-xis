14f6f2d49c74b1571af3b152602cfe1691da93b5
Marcelo Garcias <garcias.marcelo98@proton.me>
2026-03-15 09:29:22 -0300

    feat(models): define interfaces e modelos de dados do sistema
    Define estruturas de dados tipadas para:
    - About: Informações institucionais.
    - Info: Dados gerais de configuração.
    - StrapiResponse: Estrutura padrão de resposta da API do CMS.

---
+++ front-end/src/app/models/info.model.ts
@@ -0,0 +1,11 @@
+// src/app/cms/models/info.model.ts
+
+export interface Info {
+  email: string;
+  cnpj: string;
+  copyrightYear: string;
+  whatsappRaw: string;
+  whatsappFormatted: string;
+  instagramFormatted: string;
+  instagramRaw: string;
+}
