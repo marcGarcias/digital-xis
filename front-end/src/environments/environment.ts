19db9bcf253a148ede21ff4ff9b1028d32784c6e
Marcelo Garcias <garcias.marcelo98@proton.me>
2026-08-03 16:00:37 -0300

    Delete front-end directory

--- front-end/src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:8080/api',
-    serverUrl: 'http://localhost:8080',
-    turnstileSiteKey: '0x4AAAAAACq-vuBovbvO9j03',
-    googleAnalyticsId: 'G-6KLYZQT3SV'
-};
